01042942c391f5d1cde226b26b30b80f2a9386d7
Kamil Sorokosz <ksorokosz@wp.pl>
2016-01-30 17:19:02 +0100

    Basic C++ presentation and code added

---
+++ Kod/Basic/zmienne.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/* Program przedstawiający sposób tworzenia zmiennych w C++ */
+
+int main()
+{
+	short a = 128;
+	int b = 123456;
+	long c = 12345678910L;
+	
+	float f = 1.23;
+	double e = 1.23456789;
+
+	cout << "Short: " << a <<" Integer: ";
+	cout << b << " Long: " << c << " Float: " << f << " Double: " << e;
+	cout << endl;
+
+	return 0;
+}
