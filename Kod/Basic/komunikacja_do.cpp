34ce0afeee98f22708c750bbd1c210ef6d5a35c7
Kamil Sorokosz <mail@mail>
2016-02-12 20:06:33 +0100

    Lesson 2 added

--- Kod/Basic/komunikacja_do.cpp
+++ Kod/Basic/komunikacja_do.cpp
@@ -1,12 +1,16 @@
+#include <iostream> // w³¹czenie biblioteki (#include), <iostream> - nazwa biblioteki
+using namespace std; // przestrzeñ nazw biblioteki standardowej
 
-#include <iostream>
-using namespace std;
+/* to
+jest
+komentarz podzielny
+*/
 
 int main()
-{
-	cout << "Hello. I'm C++";
-	cout << " .";
-	cout << endl;
-
+{	
+	cout << "as" << endl; // oddzielna linijka
+	cout << "df" << endl; // oddzielna linijka
+	cout << "as";           // to kolejna linjka
+	cout << "df" << endl;   // a to jest w tej samej co poprzednia
 	return 0;
 }
