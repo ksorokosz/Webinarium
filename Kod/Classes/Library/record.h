a153fec0ff1163bfe03b2e2b69e62ad5258df5dc
Kamil Sorokosz <mail@mail>
2016-03-06 13:09:43 +0100

    lesson 3,4,5 added

---
+++ Kod/Classes/Library/record.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// struktura
+// pierwowzór klas
+// pochodzi z Jêzyka C
+struct Date
+{
+	int day;
+	int month;
+	int year;
+};
+
+// struktura
+// pierwowzór klas
+// pochodzi z Jêzyka C
+struct Record
+{
+	string name;
+	string surname;
+
+	Date deadline;	
+};
